c6143ffccc8e0cb7840fc14f0bb3c40768e97f6f
Yogesh Sharma <iyogesharma@gmail.com>
2024-06-21 11:42:04 +0530

    set db_connection from env

--- src/config/multidb.php
+++ src/config/multidb.php
@@ -4,5 +4,5 @@ return [
     
     'new_migrations_path' => env('NEW_MIGRATIONS_PATH','app/newdatabase/migrations'),
 
-    'db_connection' => 'mysql'
+    'db_connection' => env('DB_CONNECTION', 'mysql')
 ];
